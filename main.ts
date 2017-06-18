85024508a0ddf265cee3cd484191bcf72c320a0b
Sunil <sunil10790@gmail.com>
2017-06-18 15:37:11 +0530

    Remove Observer object and replace it with functions

--- main.ts
+++ main.ts
@@ -3,16 +3,8 @@ import { Observable, Observer } from 'rxjs';
 let numberArray = [1, 6, 10, 37];
 let source = Observable.from(numberArray);
 
-class numberObserver implements Observer<number> {
-    next(value) {
-        console.log(`Value: ${value}`);
-    }
-    error(e) {
-        console.log(`Error: ${e}`);
-    }
-    complete() {
-        console.log(`Complete`);
-    }
-}
-
-source.subscribe(new numberObserver());
+source.subscribe(
+    (value) => console.log(`Value: ${value}`),
+    (e) => console.log(`Error: ${e}`),
+    () => console.log(`Complete`)
+);
