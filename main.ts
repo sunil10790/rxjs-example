a34c7ae176b52ff61d8d581d38f1c8aad2fc44f1
Sunil <sunil10790@gmail.com>
2017-06-18 12:25:45 +0530

    Using an Observable and Observer

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-alert("Mogabo Kush Hua!")
+import { Observable, Observer } from 'rxjs';
+
+let numberArray = [1, 6, 10, 37];
+let source = Observable.from(numberArray);
+
+class numberObserver implements Observer<number> {
+    next(value) {
+        console.log(`Value: ${value}`);
+    }
+    error(e) {
+        console.log(`Error: ${e}`);
+    }
+    complete() {
+        console.log(`Complete`);
+    }
+}
+
+source.subscribe(new numberObserver());
