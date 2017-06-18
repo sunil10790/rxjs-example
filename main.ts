46871116c9c290b5da69bd10669c3469db396670
Sunil <sunil10790@gmail.com>
2017-06-18 16:00:33 +0530

    Use Observable.create() instead of Observable.from()

--- main.ts
+++ main.ts
@@ -1,7 +1,14 @@
 import { Observable, Observer } from 'rxjs';
 
 let numberArray = [1, 6, 10, 37];
-let source = Observable.from(numberArray);
+let source = Observable.create(
+    observer => {
+        for (let number of numberArray) {
+            observer.next(number);
+        }
+        observer.complete();
+    }
+);
 
 source.subscribe(
     (value) => console.log(`Value: ${value}`),
